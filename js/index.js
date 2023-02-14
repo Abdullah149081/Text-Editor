65f99cf9f72d1d8a67cf33bca3854631ab1135ff
Abdullah149081 <masudmd425@gmail.com>
2023-02-14 20:08:51 +0600

    Text Editor

---
+++ js/index.js
@@ -0,0 +1,27 @@
+document.getElementById("bold").addEventListener("click", function () {
+  bold("text-write");
+});
+
+document.getElementById("italic").addEventListener("click", function () {
+  italic("text-write");
+});
+
+document.getElementById("underline").addEventListener("click", function () {
+  underline("text-write");
+});
+
+document.getElementById("left").addEventListener("click", function () {
+  left("text-write");
+});
+
+document.getElementById("center").addEventListener("click", function () {
+  center("text-write");
+});
+
+document.getElementById("right").addEventListener("click", function () {
+  right("text-write");
+});
+
+document.getElementById("justify").addEventListener("click", function () {
+  justify("text-write");
+});
---
+++ js/utility.js
@@ -0,0 +1,28 @@
+function justify(justifyText) {
+  const justifyValue = document.getElementById(justifyText);
+  justifyValue.style.textAlign = "justify";
+}
+function bold(boldText) {
+  const fontBoldValue = document.getElementById(boldText);
+  fontBoldValue.style.fontWeight = "bold";
+}
+function italic(italicText) {
+  const italicValue = document.getElementById(italicText);
+  italicValue.style.fontStyle = "italic";
+}
+function underline(underlineText) {
+  const underlineValue = document.getElementById(underlineText);
+  underlineValue.style.textDecoration = "underline";
+}
+function left(leftTExt) {
+  const leftValue = document.getElementById(leftTExt);
+  leftValue.style.textAlign = "left";
+}
+function center(centerText) {
+  const leftValue = document.getElementById(centerText);
+  leftValue.style.textAlign = "center";
+}
+function right(rightText) {
+  const rightValue = document.getElementById(rightText);
+  rightValue.style.textAlign = "right";
+}
